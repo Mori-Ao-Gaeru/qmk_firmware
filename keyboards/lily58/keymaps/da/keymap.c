8c8bd8e48d0690f71efff2b1da947fd06d7feebd
Frog <parechutefrog@gmail.com>
2020-08-16 09:11:11 +0900

    fix adjust layer minus key

--- keyboards/lily58/keymaps/da/keymap.c
+++ keyboards/lily58/keymaps/da/keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT( \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_7  , KC_8  , KC_9  , KC_SLSH, KC__VOLUP, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_4  , KC_5  , KC_6  , KC_ASTR, KC__VOLDOWN, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_1  , KC_2  , KC_3  , RGB_SAI, KC_PGUP	, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_1  , KC_2  , KC_3  , KC_MINS, KC_PGUP	, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0  , KC_DOT, KC_EQL, KC_PLUS, KC_PGDN,\
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
   )
